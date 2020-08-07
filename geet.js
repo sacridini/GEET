8cda30d65a9f43de5f244f7e256a840603d5e938
sacridini <eduardolacerdageo@gmail.com>
2020-08-07 15:17:08 -0300

    0.7.3

--- geet.js
+++ geet.js
@@ -1,7 +1,7 @@
 /** 
  * Google Earth Engine Toolbox (GEET)
  * Description: Lib to write small EE apps or big/complex apps with a lot less code.
- * Version: 0.7.2
+ * Version: 0.7.3
  * Eduardo Ribeiro Lacerda <elacerda@id.uff.br>
  */
 
